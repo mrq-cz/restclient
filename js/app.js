bac5b130f2e41e0193d9151ed04abd00ed999396
Lumir Mrkva <lumir.mrkva@topmonks.com>
2014-01-31 14:46:10 +0100

    sending requests

--- js/app.js
+++ js/app.js
@@ -15,11 +15,9 @@ Response = Message.extend({
 Call = Ember.Object.extend({
     method: 'GET',
     url: '',
-    request: Message.create(),
-    response: Response.create()
+    request: {},
+    response: {}
 });
-
-
 
 //.. controller ............................................
 
@@ -30,31 +28,75 @@ App.IndexController = Ember.Controller.extend({
     url: '',
     method: 'GET',
 
+    body: '',
+
     selected: Call.create(),
 
-    select: function(call) {
-        this.set('selected', call);
-    },
+    actions : {
+        select: function(call) {
+            this.set('selected', call);
+            this.set('url',call.url);
+            this.set('method',call.method);
+            this.set('body',call.request.body);
+        },
 
-    remove: function(call) {
-        if (this.selected == call) this.set('selected', null);
-        History.removeObject(call);
-    },
+        remove: function(call) {
+            if (this.selected == call) this.set('selected', null);
+            History.removeObject(call);
+        },
 
-    call: function() {
-        var call = Call.create({url:this.url,method:this.method});
-        this.set('selected', call);
-        History.addObject(call);
-        this.url = '';
+        call: function() {
+            var call = Call.create(
+                {
+                    url: this.url,
+                    method: this.method,
+                    request: Message.create({
+                        body: this.body
+                    }),
+                    response: Response.create()
+                }
+            );
+            call.get('request').set('body', this.body);
+            this.set('selected', call);
+            History.addObject(call);
+            $.ajax({
+                url: this.url,
+                type: this.method,
+                success: function(data, status) {
+                    var body;
+                    if (typeof data == "object") {
+                        body = JSON.stringify(data, undefined, 2);
+                    } else {
+                        body = data;
+                    }
+                    call.set('response.body',body);
+                },
+                complete: function(xhr, status) {
+                    call.set('response.code',xhr.status);
+                }
+
+            });
+        },
+
+        clean: function() {
+            this.setProperties({url: '', method: 'GET', body: ''})
+        }
     }
 });
 
+
+//.. view ..................................................
+
+CodeMirrorView = Ember.TextArea.extend({
+    didInsertElement: function() {
+        this.codeMirror = CodeMirror.fromTextArea(this.get('element'));
+    }
+});
 
 
 //.. router ................................................
 
 App.Router.map(function() {
-    // put your routes here
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -62,10 +104,5 @@ App.IndexRoute = Ember.Route.extend({
         var call = Call.create();
         call.set('url','url0');
         History.addObjects([call, Call.create({url:'url1'}), Call.create().set('url','url2')]);
-        //controller.set('history', this.model());
-    },
-
-    model: function() {
-        return [];
     }
 });
